0abdd5bb6dbf7ad78108b634a63cec059dad8a89
Mumblecore <alanmonroyb@gmail.com>
2020-03-12 11:00:46 -0500

    optimizacion

--- vuelto.cpp
+++ vuelto.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
     //MONEDAS
-    int ent[] = {200, 100, 50, 20, 10, 5, 2, 1};
-    float dec[] = {0.5, 0.2, 0.1};
+    float mon[] = {2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
 
     float n;
 
@@ -14,27 +13,14 @@ int main()
     cin >> n;
     cout << "\nmonto\tcant.\n";
 
-    int n_int = int(n);
-    float n_dec = n - n_int;
+    int n_int = int(n*10);
 
     for(int i = 0; n_int > 0; i++)
     {
-        int v = n_int/ent[i];
-        if(v){cout << ent[i] << "\t" << v << "\n";}
-        n_int -= v*ent[i];
+        int v = n_int/mon[i];
+        if(v){cout << mon[i]/10 << "\t" << v << "\n";}
+        n_int -= v*mon[i];
     }
-
-    for(int i = 0; i < 3; i++)
-    {
-        int v = 0;
-        while(n_dec >= dec[i])
-        {
-            v++;
-            n_dec -= dec[i];
-        }
-        if(v){cout << dec[i] << "\t" << v << "\n";}
-    }
-
 
     return 0;
 }
