0d15de2d32c7e0b4d2b5b28a794cefe04de10ee7
Mumblecore <alanmonroyb@gmail.com>
2020-03-29 14:50:50 -0500

    func

--- numero_a_letras.cpp
+++ numero_a_letras.cpp
@@ -18,7 +18,12 @@ string cen[10]=
   "cien ", "ciento ", "doscientos ", "trescientos ", "cuatrocientos ", "quinientos ", "seiscientos ", "setecientos ", "ochocientos ", "novecientos "
 };
 
-int trio_num(u_long n)
+string mill[6]=
+{
+    " millones ", " mil ", " billones ", " un millon " ,"\0", " un billon "
+};
+
+int trio_num(long n)
 {
     int c = 1;
     while(n/1000)
@@ -29,25 +34,26 @@ int trio_num(u_long n)
     return c;
 }
 
-int trio_f(u_long n, int x)
+
+int trio_f(long n, int x)
 {
-    u_long m = 1000;
-    u_long d = 1;
-    
+    long m = 1000;
+    long d = 1;
+
     while(x>1)
     {
         m *= 1000;
         d *= 1000;
         x--;
     }
-    
+
     return int((n%m)/d);
 }
 
 string trio_anal(int x)
 {
     string trio = "";
-    
+
     if(x)
     {
         if(x == 100){return cen[0];}    //evaluar cien
@@ -55,8 +61,8 @@ string trio_anal(int x)
         {
             trio += cen[x/100];
         }
-        
-        if(x%100 >= 15)                 //evaluar decenas
+
+        if(x%100 > 15)                 //evaluar decenas
         {
             if(x%100 < 20)
             {
@@ -76,62 +82,46 @@ string trio_anal(int x)
                     trio = trio + "y " + uni[x%10];
                 }
             }
-        }
-        else
-        {
-            trio += uni[x];
-            
+        }else{
+
+            trio += uni[x%100];
         }
     }
-    
+
     return trio;
 }
 
 int main()
 {
     cout << "Ingrese: ";
-    u_long n;
+    long n;
     cin >> n;
-  
-    if(trio_num(n) > 4)
+
+    for(int i = 4; i > 2; i-=2)
     {
-        if(trio_f(n,6))
+        if(trio_num(n) >  i)
         {
-            cout << trio_anal(trio_f(n,4)) << " mil ";
-        }
-        if(trio_f(n,5))
-        {
-            if(trio_f(n,5) == 1)
+            if(trio_f(n,i+2))
             {
-                cout << "un billon ";
-            }else{
-                cout << trio_anal(trio_f(n,5)) << " billones ";
+                cout << trio_anal(trio_f(n,i+2)) << mill[1];
+            }
+            if(trio_f(n,i+1))
+            {
+                if(trio_f(n,i+1) == 1)
+                {
+                    cout << mill[i+1];
+                }else{
+                    cout << trio_anal(trio_f(n,i+1)) << mill[i-2];
+                }
             }
         }
     }
-    
-    if(trio_num(n) > 2)
-    {
-        if(trio_f(n,4))
-        {
-            cout << trio_anal(trio_f(n,4)) << " mil ";
-        }
-        if(trio_f(n,3))
-        {
-            if(trio_f(n,3) == 1)
-            {
-                cout << "un millon ";
-            }else{
-                cout << trio_anal(trio_f(n,3)) << " millones ";
-            }
-        }
-    }
-    
+
     if(trio_f(n,2))
     {
         cout << trio_anal(trio_f(n,2)) << " mil ";
     }
     cout << trio_anal(trio_f(n,1)) << endl;
-    
+
     return 0;
 }
